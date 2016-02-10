693e9cc0c16394a6970d02982534e72f59fc0f6e
Germán Jiménez <german.jimenez@genetsis.com>
2016-02-10 14:26:55 +0100

    do not make docker-compose.yml accesible

--- docker/druid-ini.sh
+++ docker/druid-ini.sh
@@ -8,7 +8,6 @@ DOCUMENT_ROOT="$WEB_ROOT/webapp"
 if [ ! -d $DOCUMENT_ROOT ]; then
     echo "copy DRUID to DOCUMENT_ROOT $DOCUMENT_ROOT"
     (cd $WEB_SRC; tar cvf - .) | (cd $WEB_ROOT; tar xvf -)
-    cp $SRC/*.yml $WEB_ROOT
     echo "Setting owner and permissions"
     chown -R www-data:www-data $WEB_ROOT
 fi
