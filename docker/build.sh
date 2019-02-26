f9e8260a44856fe4bf53649cf6d24575dc1bc88f
German Jimenez <german.jimenez@genetsis.com>
2019-02-26 12:01:34 +0100

    upgrading versions

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-TAG5="druidcom/sdk-v1.0.4_php5.6_apache"
-TAG7="druidcom/sdk-v1.0.4_php7.0_apache"
+TAG56="druidcom/sdk-v1.0.5_php5.6_apache"
+TAG70="druidcom/sdk-v1.0.5_php7.0_apache"
+TAG71="druidcom/sdk-v1.0.5_php7.1_apache"
 
-docker build -f Dockerfile-5.6 --no-cache -t $TAG5 .
-docker push $TAG5
+#docker build -f Dockerfile-5.6 --no-cache -t $TAG56 .
+#docker push $TAG56
 
-docker build -f Dockerfile-7.0 --no-cache -t $TAG7 .
-docker push $TAG7
+#docker build -f Dockerfile-7.0 --no-cache -t $TAG70 .
+#docker push $TAG70
+
+docker build --build-arg SDK_VERSION=v1.0.5 -f Dockerfile-7.1 --no-cache -t $TAG71 .
+docker push $TAG71
